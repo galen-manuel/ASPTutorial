930b40a7fe1bf53db5169fb2f047d7f201cb9f39
Galen Manuel <galen.manuel@gmail.com>
2020-05-08 10:27:39 -0400

    Went through to 6:30 in video 6/13

--- Tutorial/Tutorial.Website/Pages/Index.cshtml.cs
+++ Tutorial/Tutorial.Website/Pages/Index.cshtml.cs
@@ -5,21 +5,27 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Tutorial.Website.Models;
+using Tutorial.Website.Services;
 
 namespace Tutorial.Website.Pages
 {
     public class IndexModel : PageModel
     {
+        public readonly JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+
         private readonly ILogger<IndexModel> _logger;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(ILogger<IndexModel> logger, JsonFileProductService productService)
         {
             _logger = logger;
+            ProductService = productService;
         }
 
         public void OnGet()
         {
-
+            Products = ProductService.GetProducts();
         }
     }
 }
--- Tutorial/Tutorial.Website/Startup.cs
+++ Tutorial/Tutorial.Website/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Tutorial.Website.Services;
 
 namespace Tutorial.Website
 {
@@ -23,6 +24,7 @@ namespace Tutorial.Website
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<JsonFileProductService>();
             services.AddRazorPages();
         }
 
